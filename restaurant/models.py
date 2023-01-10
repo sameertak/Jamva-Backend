5f4bb20c5f66bd15650309e8acfb9a3e06e7ce5b
taksa <taksamir08@gmail.com>
2023-01-10 14:58:56 +0530

    Created certain APIs for handling Menu Page

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+from userLogin.models import phoneModel
 
 # Create your models here.
+class restaurantModel(models.Model):
+    name = models.CharField(blank=False, max_length=25)
+    manager = models.CharField(max_length=25)
+    contact = models.CharField(max_length=12, blank=False)
+    email = models.EmailField(max_length=35)
+    social = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
+class menuModel(models.Model):
+    resId = models.CharField(max_length=3, blank=False)
+    category = models.CharField(max_length=15, blank=False)
+    foodName = models.CharField(max_length=25, blank=False)
+    description = models.CharField(max_length=50, default='Street Food, Gujarati')
+    price = models.IntegerField(blank=False)
+    ratings = models.IntegerField(default=0)
+    counter = models.IntegerField(default=0)
+    image = models.ImageField(default='default.png')
+
+    def __str__(self):
+        return str(self.foodName)
+
+
+class categoryModel(models.Model):
+    resId = models.CharField(max_length=3, blank=False)
+    category = models.CharField(max_length=15, blank=False)
+    catImage = models.ImageField(default='default.png')
+
+    def __str__(self):
+        return str(self.category)
+
+
+class orderModel(models.Model):
+    userId = models.IntegerField(default=1)
+    foodId = models.IntegerField(default=1)
+    resId = models.CharField(default=1, max_length=3)
+    quantity = models.IntegerField(default=1)
+    status = models.BooleanField(default=False)
+    price = models.IntegerField(default=1)
+
+    def __str__(self):
+        return str(self.userId)
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,212 @@
-from django.shortcuts import render
+import time
 
-# Create your views here.
+from django.db.migrations import serializer
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.views import APIView
+from .models import menuModel, restaurantModel, categoryModel, orderModel
+from .serializers import MenuSerializers, RestaurantSerializers, CategorySerializers, CategoryMenuSerializers, \
+    OrderSerializers
+
+
+class RestMenu(APIView):
+    permission_classes = [AllowAny]
+
+    @staticmethod
+    def post(request):
+        res = request.data
+        q = "SELECT m.* FROM restaurant_menumodel m INNER JOIN restaurant_restaurantmodel r ON m.resId=r.id WHERE r.id=" + "'"+ str(res['resId']) + "'"
+        queryset = menuModel.objects.raw(q)
+        serializer = MenuSerializers(queryset, many=True)
+
+        return Response(
+            status=status.HTTP_200_OK,
+            data={
+                "data" : serializer.data
+            }
+
+        )
+
+
+class RestDetail(APIView):
+    permission_classes = [AllowAny]
+
+    @staticmethod
+    def post(request):
+        res = request.data
+
+        q = "SELECT * FROM restaurant_restaurantmodel WHERE id='"+str(res['resId'])+"'"
+        queryset = restaurantModel.objects.raw(q)
+        serializer = RestaurantSerializers(queryset, many=True)
+
+        return Response(
+            status=status.HTTP_200_OK,
+            data={
+                "data" : serializer.data
+            }
+        )
+
+
+class AvailabeCategory(APIView):
+
+    permission_classes = [AllowAny]
+
+    @staticmethod
+    def post(request):
+        res = request.data
+        q = "SELECT * FROM restaurant_categorymodel WHERE resId='" + str(res['resId']) + "'"
+        queryset = categoryModel.objects.raw(q)
+        serializer = CategorySerializers(queryset, many=True)
+        return Response(
+            status=status.HTTP_200_OK,
+            data = {
+                "data" : serializer.data
+            }
+        )
+
+
+class AddMenu(APIView):
+    permission_classes = [AllowAny]
+
+    @staticmethod
+    def post(request):
+        res = request.data
+
+        if list(menuModel.objects.filter(resId=res['resId'])):
+            if (list(menuModel.objects.filter(foodName=res['foodName']))) == []:
+                if 'image' in res.keys():
+                    menuModel.objects.create(
+                        resId = res['resId'],
+                        category=res['category'],
+                        foodName=res['foodName'],
+                        description=res['description'],
+                        price=res['price'],
+                        image=res['image']
+                    )
+                else:
+                    menuModel.objects.create(
+                        resId=res['resId'],
+                        category=res['category'],
+                        description=res['description'],
+                        foodName=res['foodName'],
+                        price=res['price'],
+                    )
+            else:
+                return Response(
+                    data={"data": "Food Name Already Exits"}
+                )
+
+            try:
+
+                if list(categoryModel.objects.filter(category=res['category'])) == []:
+
+                    if 'catImage' in res.keys():
+                        categoryModel.objects.create(
+                            resId=res['resId'],
+                            category=res['category'],
+                            catImage=res['catImage']
+                        )
+                    else:
+                        categoryModel.objects.create(
+                            resId=res['resId'],
+                            category=res['category'],
+                        )
+            except:
+                return Response(
+                    data={
+                        "data" : "Category Already Exists"
+                    }
+                )
+
+            return Response(
+                status=status.HTTP_200_OK,
+                data={
+                    "msg" : "Menu Successfully Updated!!"
+                }
+            )
+        else:
+            return Response(
+                status=status.HTTP_404_NOT_FOUND,
+                data ={
+                    "msg" : "Restaurant Not Found!!"
+                }
+            )
+
+
+class CategoryMenu(APIView):
+    permission_classes = [AllowAny]
+
+    @staticmethod
+    def post(request):
+        res = request.data
+
+        try:
+            q = "SELECT * FROM restaurant_menumodel WHERE resId='"+res['resId']+"' AND category='"+res['category']+"'"
+            queryset = menuModel.objects.raw(q)
+            serializer = CategoryMenuSerializers(queryset, many=True)
+
+            return Response(
+                status=status.HTTP_200_OK,
+                data={
+                    "data" : serializer.data
+                }
+            )
+
+        except:
+            return Response(
+                status=status.HTTP_404_NOT_FOUND
+            )
+
+
+class UserOrder(APIView):
+    permission_classes = [AllowAny]
+
+    @staticmethod
+    def post(request):
+        global serializer
+
+        res = request.data
+
+        orderModel(
+            userId=res['userId'],
+            resId=res['resId'],
+            foodId=res['foodId'],
+            quantity=res['quantity'],
+            price=res['price']
+        ).save()
+
+        q = f"SELECT * FROM restaurant_ordermodel WHERE status=False AND userId='{res['userId']}' AND resId='{res['resId']}'"
+        queryset = orderModel.objects.raw(q)
+        serializer = OrderSerializers(queryset, many=True)
+        totalQuantity, totalPrice= 0, 0
+
+        for i in serializer.data:
+            totalQuantity+=i['quantity']
+            totalPrice += i['price']
+
+        return Response(
+            status=status.HTTP_200_OK,
+            data={
+                "data" : {
+                    "totalQuantity" : totalQuantity,
+                    "totalPrice" : totalPrice
+                }
+            }
+        )
+
+    # @staticmethod
+    # def post(request):
+    #     res = request.data
+    #     print(res)
+    #     temp = ""
+    #     for id_ in res['oderedItems']:
+    #         id_.value()
+    #         temp+=f"{id},"
+    #         print(temp)
+    #     return Response(
+    #         data={
+    #             "data" : res
+    #         }
+    #     )
