879d9d266f3327ec5c7c3147de481906ee182c51
taksa <taksamir08@gmail.com>
2023-01-10 14:59:57 +0530

    Handled OTP Authentication and moved confedential data to env files

--- userLogin/urls.py
+++ userLogin/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import otpLogin
+from .views import OtpLogin
 
 urlpatterns = [
-    path("<phone>/<name>", otpLogin.as_view()),
+    path("<phone>/<name>", OtpLogin.as_view()),
+    # path("/details", userDetail.as_view())
 ]
