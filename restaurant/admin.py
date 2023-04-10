e7706ac9f8d7d2a1895a083d732d8547ed9d3a1e
samirtak <taksamir08@gmail.com>
2023-04-10 18:24:44 +0530

    Registered the new Model in the application for admin panel

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import restaurantModel, menuModel, categoryModel, orderModel
+from .models import restaurantModel, menuModel, categoryModel, orderModel, employeeModel
 
 admin.site.register(restaurantModel)
 admin.site.register(menuModel)
 admin.site.register(categoryModel)
 admin.site.register(orderModel)
+admin.site.register(employeeModel)
