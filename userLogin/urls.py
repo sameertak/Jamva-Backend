4dd1ae371e2c73b30f44c4b5feb00016c1739ea2
samirtak <taksamir08@gmail.com>
2023-04-10 18:29:49 +0530

    Added new Routes for the API for the ordermodel

--- userLogin/urls.py
+++ userLogin/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import OtpLogin, userVerify
+from .views import OtpLogin, userVerify, Logout
 
 urlpatterns = [
     path("<phone>/<name>", OtpLogin.as_view()),
-    # path("verify/", userVerify.as_view())
+    path("verify/", userVerify.as_view()),
+    path("logout/", Logout.as_view())
 ]
