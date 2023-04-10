288dfc0f501fe13370cda126fc75cbf57b8c4374
samirtak <taksamir08@gmail.com>
2023-04-10 18:23:55 +0530

    Added new API routes for tokenization and verification of the user

--- Jamva/urls.py
+++ Jamva/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import MyTokenObtainPairView
+from .views import MyTokenObtainPairView, RegisterHere
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('register/', RegisterHere.as_view(), name='Register Manager'),
     path('userlogin/', include('userLogin.urls')),
     path('restaurant/', include('restaurant.urls')),
-    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair')
+    path('token/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    # path('payment/', Payment.as_view())
 ]
 if settings.DEBUG:
      urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
